868d10850a901a2fee7d1d88b934091241584957
Junebhone <bhonekyawsihein@gmail.com>
2022-03-17 13:49:34 +0630

    country done

--- resources/views/admin/countries/create.blade.php
+++ resources/views/admin/countries/create.blade.php
@@ -1,4 +1,4 @@
-<x-admin-layout>
+{{-- <x-admin-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('New Country') }}
@@ -63,6 +63,134 @@
                                             <button type="submit"
                                                 class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
                                         </div>
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</x-admin-layout> --}}
+
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('New Country') }}
+        </h2>
+    </x-slot>
+
+    <div class="container mx-auto ">
+        <!-- This example requires Tailwind CSS v2.0+ -->
+        <div class="flex flex-col">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:mx-8">
+                <div class="align-middle inline-block w-full sm:px-6 lg:px-8">
+                    <div class="flex justify-between">
+                        <div class=" items-center py-4 overflow-y-auto flex sm:flex whitespace-nowrap">
+                            <a href="{{ route('admin.index') }}" class="text-gray-600 dark:text-gray-200">
+                                <lottie-player src="{{ asset('img/Home.json') }}" background="transparent" speed="1"
+                                    class="w-6 h-6" hover>
+                                </lottie-player>
+                            </a>
+                            <span class="mx-1 text-gray-500 dark:text-gray-300">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20"
+                                    fill="currentColor">
+                                    <path fill-rule="evenodd"
+                                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </span>
+                            <a href="{{ route('admin.countries.index') }}" class="text-black hover:underline">
+                                Countries
+                            </a>
+                            <span class="mx-1 text-gray-500 dark:text-gray-300">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20"
+                                    fill="currentColor">
+                                    <path fill-rule="evenodd"
+                                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </span>
+                            <a href="{{ route('admin.countries.create') }}" class="text-black hover:underline">
+                                Create-Country
+                            </a>
+                            <span class="mx-1 text-gray-500 dark:text-gray-300">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20"
+                                    fill="currentColor">
+                                    <path fill-rule="evenodd"
+                                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                            </span>
+                        </div>
+                        <a href="{{ route('admin.countries.index') }}"
+                            class="text-black flex py-4 justify-center items-center">
+                            <span class="text-black hover:underline">Back</span>
+                            <lottie-player src="{{ asset('img/Back.json') }}" background="transparent" speed="1"
+                                class="w-16 h-16" hover>
+                            </lottie-player>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="overflow-x-auto sm:-mx-6 lg:mx-8">
+                <div class="align-middle inline-block w-full sm:px-6 lg:px-8">
+                    <div class="overflow-hidden p-2">
+                        <div>
+                            <div class="md:grid md:grid-cols-3 md:gap-6">
+                                {{-- <div class="md:col-span-1">
+                                    <div class="px-4 sm:px-0">
+                                        <h3 class="text-lg font-medium leading-6 text-gray-900">Create Category</h3>
+                                    </div>
+                                </div> --}}
+                                <div class="mt-5 md:mt-0 md:col-span-3">
+                                    <form action="{{ route('admin.countries.store') }}" method="POST"
+                                        enctype="multipart/form-data">
+                                        @csrf
+                                        <div class="shadow rounded-md sm:rounded-md  sm:overflow-hidden">
+                                            <div class="rounded-md px-4 py-5 bg-white space-y-6 sm:p-6">
+                                                <div class="grid grid-cols-3 gap-6">
+                                                    <div class="col-span-3 sm:col-span-3">
+                                                        <label for="name"
+                                                            class="py-2 block text-sm font-medium text-black">
+                                                            Name
+                                                        </label>
+                                                        <div class="mt-1 flex rounded-md shadow-sm">
+                                                            <input type="text" name="name" id="name"
+                                                                class="border-gray-300 focus:border-[#FF385C]
+                                                                focus:ring focus:ring-red-300 focus:ring-opacity-50 rounded-md shadow-sm flex-1 block w-full sm:text-sm "
+                                                                placeholder="Name">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="grid grid-cols-3 gap-6">
+                                                    <div class="col-span-3 sm:col-span-3">
+                                                        <label for="country_code"
+                                                            class="block text-sm font-medium text-gray-700"> Country
+                                                            Code
+                                                        </label>
+                                                        <div class="mt-1 flex rounded-md shadow-sm">
+                                                            <input type="text" name="country_code" id="country_code"
+                                                                class="border-gray-300 focus:border-[#FF385C]
+                                                                focus:ring focus:ring-red-300 focus:ring-opacity-50 rounded-md shadow-sm flex-1 block w-full sm:text-sm "
+                                                                placeholder="Country Code">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="px-4 py-3 flex justify-end  text-left sm:px-6">
+                                                <button type="submit" class="inline-flex items-center px-4 py-2 bg-[#FF385C] border
+                                                    border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-rose-600
+                                                    active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25
+                                                    transition">Save</button>
+                                            </div>
+                                        </div>
+
 
                                     </form>
                                 </div>
